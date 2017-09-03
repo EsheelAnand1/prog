2a65d6255889625daebf306624137f0ecbabeb04
EsheelAnand1 <esheelroy@gmail.com>
2017-09-03 20:11:46 +1200

    first commit

---
+++ Script.js
@@ -0,0 +1,29 @@
+/*let fname = prompt("Please enter your first name", "Firstname")
+alert(fname)
+
+let lname = prompt("Please enter your first name", "Lastname")
+alert(lname)*/
+
+let timestables = prompt("What times table do you want to see", "00")
+for (var i = 1; i <= 12; i++) {
+
+    tablestables = parseInt(timestables)
+
+    let a = i + 1
+    console.log(a + " x "+ timestables + " = " + (a*timestables))
+
+
+}
+
+/*let number = 0
+let count = 5`
+
+for (var i = 1; i <= count; i++) {
+
+    let answer = prompt("Please enter number ", "00")
+    number += parseInt(answer)
+
+
+}
+console.log("The total number is = " + number);
+console.log("The total Average is = " + number / count);*/
